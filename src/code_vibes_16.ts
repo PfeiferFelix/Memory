80be109bc755de9d61684c4ae73da6f071592ff1
Felix Pfeifer <felix@pr-media.at>
2026-08-01 00:28:57 +0200

    Implement popup functionality for game exit and navigation

--- src/code_vibes_16.ts
+++ src/code_vibes_16.ts
@@ -53,6 +53,30 @@ field.innerHTML = werte
 
 init(auswahl.length);
 
+// Popup
+const popup = document.getElementById("popup")!;
+const openPopupButton = document.getElementById("openPopupButton")!;
+const backToGameButton = document.getElementById("backToGameButton")!;
+const exitToMenuButton = document.getElementById("exitToMenuButton")!;
+
+openPopupButton.addEventListener("click", openPopup);
+
+function openPopup() {
+    popup.classList.remove("none");
+}
+
+backToGameButton.addEventListener("click", closePopup);
+
+function closePopup() {
+    popup.classList.add("none");
+}
+
+exitToMenuButton.addEventListener("click", exitToMenu);
+
+function exitToMenu() {
+    window.location.href = "setting.html";
+}
+
 
 
 
